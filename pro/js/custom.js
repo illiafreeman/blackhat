7a044758d62b87b47ddfd37d0fbc3301e3461efc
freeman <iviewn@gmail.com>
2021-04-09 17:03:45 +0300

    landing nav

--- pro/js/custom.js
+++ pro/js/custom.js
@@ -1,66 +1,15 @@
 $(document).ready(function() {
-    $('.card_error').click(function (e) {
-        $('.preload').toggleClass('vis');
-        $('body').toggleClass('oh');
-    });
 
     $('.hambu').click(function(){
         $('.navleft').toggleClass('vis');
         $('body').toggleClass('oh');
         $('#nav-icon4').toggleClass('open');
     });
-    /*toastr options*/
-    toastr.options = {
-        closeButton: true,
-        "positionClass": "toast-bottom-right",
-        "timeOut": "300000"/*too long timeout for test*/
-    };
-    /*end toastr options*/
-    $('#agree').click(function(){
-        if($(this).prop("checked") == true){
-            $('.login').removeClass('login_dis')
-        }
-        else if($(this).prop("checked") == false){
-            $('.login').addClass('login_dis')
-        }
+    $("body").scrollspy({target: "#navigation", offset:130});
+    $('.menu').click(function () {
+        $.scrollTo($('#scroll_' + $(this).attr('id')), 1000, {axis:'y', offset:-100});
     });
 })
 
-$('#terms_link').click(function () {
-    $.scrollTo($('#terms'), 1000, {axis:'y', offset:-140});
-
-
-});
-
-
-
-
-(function () {
-    const second = 1000,
-        minute = second * 60,
-        hour = minute * 60,
-        day = hour * 24;
-
-    let birthday = "Mar 30, 2021 00:00:00",
-        countDown = new Date(birthday).getTime(),
-        x = setInterval(function() {
-
-            let now = new Date().getTime(),
-                distance = countDown - now;
-
-            document.getElementById("days").innerText = Math.floor(distance / (day)),
-                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-
-            //do something later when date is reached
-            if (distance < 0) {
-
-
-                clearInterval(x);
-            }
-            //seconds
-        }, 0)
-}());
 
 
