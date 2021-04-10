95e40f1784b9a02fa33b4a34b0c002201f8e7726
freeman <iviewn@gmail.com>
2021-04-10 20:29:19 +0300

    landing scroll

--- pro/js/custom.js
+++ pro/js/custom.js
@@ -6,8 +6,34 @@ $(document).ready(function() {
         $('#nav-icon4').toggleClass('open');
     });
     $("body").scrollspy({target: "#navigation", offset:130});
-    $('.menu').click(function () {
+/*    $('.menu').click(function () {
         $.scrollTo($('#scroll_' + $(this).attr('id')), 1000, {axis:'y', offset:-100});
+    });*/
+
+    $(".menu").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+
+            // Prevent default anchor click behavior
+            event.preventDefault();
+            var elAttr = $(this).attr('href'),
+                offset = ($(this).data('offset') ? $(this).data('offset') : 530);
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(elAttr).offset().top - offset
+            }, 800, function(){
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+
+        } // End if
+
     });
 
 
