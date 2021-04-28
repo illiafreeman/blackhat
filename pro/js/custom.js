0b2a63fa0fc2284113f0c003ffc6fed18afee54f
freeman <iviewn@gmail.com>
2021-04-28 08:14:17 +0300

    explorer main

--- pro/js/custom.js
+++ pro/js/custom.js
@@ -32,35 +32,11 @@ $('#terms_link').click(function () {
 
 });
 
-
-
-
-(function () {
-    const second = 1000,
-        minute = second * 60,
-        hour = minute * 60,
-        day = hour * 24;
-
-    let birthday = "Mar 30, 2021 00:00:00",
-        countDown = new Date(birthday).getTime(),
-        x = setInterval(function() {
-
-            let now = new Date().getTime(),
-                distance = countDown - now;
-
-            document.getElementById("days").innerText = Math.floor(distance / (day)),
-                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-
-            //do something later when date is reached
-            if (distance < 0) {
-
-
-                clearInterval(x);
-            }
-            //seconds
-        }, 0)
-}());
+$( window ).on("load", function() {
+    setTimeout(function () {
+        $('body').removeClass('oh');
+        $('.preload').removeClass('vis');
+    }, 2000);
+});
 
 
