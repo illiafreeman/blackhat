8d7a510b3fd78b9a9ac8744da3bddb97ab44fc9e
freeman <iviewn@gmail.com>
2021-06-03 18:28:13 +0300

    explorer masternodes

--- pro/js/custom.js
+++ pro/js/custom.js
@@ -39,7 +39,7 @@ $( window ).on("load", function() {
     setTimeout(function () {
         $('body').removeClass('oh');
         $('.preload').removeClass('vis');
-    }, 2000);
+    }, 1);
 });
 
 
@@ -53,5 +53,39 @@ $(document).mouseup(e => {
         $menu.removeClass('open');
     }
 });
-
-
+var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+        columns: [
+            ['data1 chart', 70],
+            ['data2 chart long', 120],
+            ['data3', 200],
+            ['data4', 550],
+        ],
+        type : 'donut'
+    },
+    donut: {
+        title: "100,700 PHC Available"
+    },
+    color: {
+        pattern: ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c', '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5', '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
+    },
+    legend: {
+        padding: 15,
+        inset: {
+            anchor: 'top-left',
+            x: 10,
+            y: 0,
+            step: undefined
+        }
+    }
+});
+$(function() {
+    $(".dial").knob({
+        'readOnly': true,
+        'format': function (value) {
+            return value + '%';
+        },
+        'fgColor': '#28d094'
+    });
+});
