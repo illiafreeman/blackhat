845ffac8e2c5419321f73a29879771dddf868c9c
freeman <iviewn@gmail.com>
2021-06-23 14:10:55 +0300

    explorer governance future

--- pro/js/custom.js
+++ pro/js/custom.js
@@ -89,39 +89,4 @@ var chart = c3.generate({
         bottom: 40
     }
 });
-$(function() {
-    $(".dial").knob({
-        'readOnly': true,
-        'format': function (value) {
-            return value + '%';
-        },
-        'fgColor': '#3a9172'
-    });
-});
-(function () {
-    const second = 1000,
-        minute = second * 60,
-        hour = minute * 60,
-        day = hour * 24;
 
-    let birthday = "Aug 30, 2021 00:00:00",
-        countDown = new Date(birthday).getTime(),
-        x = setInterval(function() {
-
-            let now = new Date().getTime(),
-                distance = countDown - now;
-
-            document.getElementById("days").innerText = Math.floor(distance / (day)),
-                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-
-            //do something later when date is reached
-            if (distance < 0) {
-
-
-                clearInterval(x);
-            }
-            //seconds
-        }, 0)
-}());
