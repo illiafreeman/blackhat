1a430d7d9901972205d67179e1d44ee5debf169d
freeman <iviewn@gmail.com>
2022-01-27 17:25:26 +0300

    git test

--- gulpfile.js
+++ gulpfile.js
@@ -18,8 +18,8 @@ gulp.task('sass', function(){
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
         .pipe(sourcemaps.write())
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
-        .pipe(cssnano())
-        .pipe(rename({suffix: '.min'}))
+        //.pipe(cssnano())
+        //.pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('pro/css'))
         .pipe(browserSync.reload({stream: true}));
 });
